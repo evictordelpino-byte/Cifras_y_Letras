717f5da749d4fbb5964fba59f3ac60cce5a10de1
Parpas16 <proparpas16@gmail.com>
2025-11-27 20:33:33 +0100

    implementación inicial diccionario

--- practica-final-cifrasyletras/include/diccionario.h
+++ practica-final-cifrasyletras/include/diccionario.h
@@ -1,10 +1,76 @@
 #ifndef __DICCIONARIO_H__
 #define __DICCIONARIO_H__
 
+#include <set>
+#include <iostream>
+#include <iofstream>
+#include <string>
+
+using namespace std;
+
 /**
  * @brief TDA DICCIONARIO
  * @details Almacena las palabras de un fichero de texto y permite iterar sobre ellas
  *
  */
+
+class DICCIONARIO {
+    private:
+        set<string> palabras; /**< Lista que almacena las palabras del diccionario */
+    public:
+        /**
+         * @brief Constructor por defecto de la clase DICCIONARIO
+         */
+        DICCIONARIO();
+
+        /**
+         * @brief Constructor de la clase DICCIONARIO que carga las palabras de un fichero
+         * @param nombre_fichero Nombre del fichero de texto que contiene las palabras
+         */
+        DICCIONARIO(const string& nombre_fichero);
+
+        /**
+         * @brief Carga las palabras de un fichero de texto en el diccionario
+         * @param nombre_fichero Nombre del fichero de texto que contiene las palabras
+         * @return true si se ha cargado correctamente, false en caso contrario
+         */
+        bool cargar_diccionario(const string &nombre_fichero);
+
+        /**
+         * @brief Muestra todas las palabras del diccionario por la salida estándar
+         */
+        void mostrar_diccionario() const;
+
+        /**
+         * @brief Busca una palabra en el diccionario
+         * @param palabra Palabra a buscar
+         * @return true si la palabra se encuentra en el diccionario, false en caso contrario
+         */
+        bool buscar(const string& palabra) const;
+
+        /**
+         * @brief Devuelve un iterador al inicio de la lista de palabras
+         * @return Iterador al inicio de la lista de palabras
+         */
+        set<string>::const_iterator begin() const;
+
+        /**
+         * @brief Devuelve un iterador al final de la lista de palabras
+         * @return Iterador al final de la lista de palabras
+         */
+        set<string>::const_iterator end() const;
+
+        /**
+         * @brief Devuelve un iterador al inicio de la lista de palabras
+         * @return Iterador al inicio de la lista de palabras
+         */
+        set<string>::iterator begin();
+
+        /**
+         * @brief Devuelve un iterador al final de la lista de palabras
+         * @return Iterador al final de la lista de palabras
+         */
+        set<string>::iterator end();
+};
 
 #endif
--- practica-final-cifrasyletras/src/diccionario.cpp
+++ practica-final-cifrasyletras/src/diccionario.cpp
@@ -1,1 +1,58 @@
 #include "diccionario.h"
+
+#include <set>
+#include <iostream>
+#include <iofstream>
+#include <string>
+
+using namespace std;
+
+DICCIONARIO::DICCIONARIO(){
+    palabras = set<string>();
+}
+
+DICCIONARIO::DICCIONARIO(const string& nombre_fichero){
+    cargar_diccionario(nombre_fichero);
+}
+
+bool DICCIONARIO::cargar_diccionario(const string &nombre_fichero){
+    palabras.clear();
+    string palabra = "";
+    istream in = ifstream(nombre_fichero);
+    if (in.fail()) {
+        cerr << "Error al abrir el fichero: " << nombre_fichero << endl;
+        return false;
+    }
+
+    while(in >> palabra){
+        palabras.insert(palabra);
+    }
+    in.close();
+
+    return true;
+}
+
+void DICCIONARIO::mostrar_diccionario() const{
+    for(auto it = palabras.begin(); it != palabras.end(); ++it){
+        cout << *it << endl;
+    }
+}
+
+bool DICCIONARIO::buscar(const string& palabra) const{
+    return palabras.find(palabra) != palabras.end();
+}
+
+set<string>::const_iterator DICCIONARIO::begin() const{
+    return palabras.cbegin();
+}
+
+set<string>::const_iterator DICCIONARIO::end() const{
+    return palabras.cend();
+}
+set<string>::iterator DICCIONARIO::begin(){
+    return palabras.begin();
+}
+
+set<string>::iterator DICCIONARIO::end(){
+    return palabras.end();
+}
